55479ef770fafac4bdf577597809bb1aad091dec
Roman Medvedev <39949986+devdem@users.noreply.github.com>
2020-04-06 16:38:51 +0300

    LoginFuncs

--- app/src/main/java/ru/devdem/sendscaledphotososerver/activites/LoginActivity.java
+++ app/src/main/java/ru/devdem/sendscaledphotososerver/activites/LoginActivity.java
@@ -1,9 +1,11 @@
 package ru.devdem.sendscaledphotososerver.activites;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,8 +17,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Response;
 
+import org.json.JSONObject;
+
 import ru.devdem.sendscaledphotososerver.R;
 import ru.devdem.sendscaledphotososerver.helpers.NetworkController;
+import ru.devdem.sendscaledphotososerver.helpers.SettingsController;
+import ru.devdem.sendscaledphotososerver.helpers.objects.*;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText etLogin;
@@ -24,11 +30,14 @@ public class LoginActivity extends AppCompatActivity {
     private ProgressBar bar;
     private Button btLogin;
     private NetworkController mNetworkController;
+    private SettingsController mSettingsController;
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mNetworkController=NetworkController.getNetworkController();
+        mNetworkController = NetworkController.getNetworkController();
+        mSettingsController = SettingsController.getInstance(this);
         View view = View.inflate(this, R.layout.activity_login, null);
         setContentView(view);
         etLogin = view.findViewById(R.id.login);
@@ -65,18 +74,48 @@ public class LoginActivity extends AppCompatActivity {
         etPassword.setEnabled(false);
 
         Response.Listener<String> listener = response -> {
-
+            try {
+                JSONObject jsonResponse = new JSONObject(response);
+                if (jsonResponse.getBoolean("password_ok") && jsonResponse.getBoolean("ok")) {
+                    JSONObject jsonUser = jsonResponse.getJSONObject("user_info");
+                    User user = new User();
+                    user.setId(jsonUser.getInt("id"));
+                    user.setName(jsonUser.getString("name"));
+                    user.setEmail(jsonUser.getString("email"));
+                    user.setLogin(jsonUser.getString("login"));
+                    user.setUrlImage(jsonUser.getString("urlImage"));
+                    user.setGroup(jsonUser.getInt("groups"));
+                    user.setPassword(jsonUser.getString("password"));
+                    user.setToken(jsonUser.getString("token"));
+                    user.setPro(jsonUser.getString("pro").equals("Yes"));
+                    mSettingsController.saveUser(user);
+                    mSettingsController.setFirst(false);
+                    Toast.makeText(this, "Успешный вход", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(this, MainActivity.class));
+                    finish();
+                } else stop("Недействительный логин или пароль");
+            } catch (Exception e) {
+                Log.e(TAG, "login: ", e);
+                stop("Произошла ошибка: " + e.getMessage());
+            }
         };
 
-        Response.ErrorListener errorListener = error -> {
-            Toast.makeText(this, "Произошла ошибка: "+error.getMessage(), Toast.LENGTH_SHORT).show();
-            bar.setVisibility(View.GONE);
-            btLogin.setEnabled(true);
-            etLogin.setEnabled(true);
-            etPassword.setEnabled(true);
-
-        };
+        Response.ErrorListener errorListener = error -> stop("Произошла ошибка: " + error.getMessage());
 
         mNetworkController.login(this, listener, errorListener, etLogin.getText().toString(), etPassword.getText().toString());
+    }
+
+    void stop(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        bar.setVisibility(View.GONE);
+        btLogin.setEnabled(true);
+        etLogin.setEnabled(true);
+        etPassword.setEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(this, FirstActivity.class));
+        finish();
     }
 }
--- app/src/main/java/ru/devdem/sendscaledphotososerver/helpers/SettingsController.java
+++ app/src/main/java/ru/devdem/sendscaledphotososerver/helpers/SettingsController.java
@@ -3,6 +3,8 @@ package ru.devdem.sendscaledphotososerver.helpers;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import ru.devdem.sendscaledphotososerver.helpers.objects.User;
+
 public class SettingsController {
     private Context mContext;
     private static SharedPreferences mSettings;
@@ -19,5 +21,37 @@ public class SettingsController {
     }
     public boolean isFirst() {
         return mSettings.getBoolean("first", true);
+    }
+
+    public void saveUser(User user) {
+        SharedPreferences.Editor editor = mAccountData.edit();
+        editor.putInt("id", user.getId());
+        editor.putString("name", user.getName());
+        editor.putString("email", user.getEmail());
+        editor.putString("login", user.getLogin());
+        editor.putString("urlImage", user.getUrlImage());
+        editor.putInt("group", user.getGroup());
+        editor.putString("password", user.getPassword());
+        editor.putString("token", user.getToken());
+        editor.putBoolean("pro", user.isPro());
+        editor.apply();
+    }
+
+    public User loadUser() {
+        User user = new User();
+        user.setId(mAccountData.getInt("id", 0));
+        user.setName(mAccountData.getString("name", ""));
+        user.setEmail(mAccountData.getString("email", ""));
+        user.setLogin(mAccountData.getString("login", ""));
+        user.setUrlImage(mAccountData.getString("urlImage", ""));
+        user.setGroup(mAccountData.getInt("group", 0));
+        user.setPassword(mAccountData.getString("password", ""));
+        user.setToken(mAccountData.getString("token", ""));
+        user.setPro(mAccountData.getBoolean("pro", false));
+        return user;
+    }
+
+    public void setFirst(boolean b) {
+        mSettings.edit().putBoolean("first", b).apply();
     }
 }
