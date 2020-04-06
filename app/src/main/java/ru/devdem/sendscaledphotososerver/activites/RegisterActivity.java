eff7ec8b3c9da8fd05b26c958ca38f14e027ea45
Roman Medvedev <39949986+devdem@users.noreply.github.com>
2020-04-06 16:50:12 +0300

    Added RegisterActivity

--- app/src/main/java/ru/devdem/sendscaledphotososerver/activites/RegisterActivity.java
+++ app/src/main/java/ru/devdem/sendscaledphotososerver/activites/RegisterActivity.java
@@ -1,13 +1,19 @@
 package ru.devdem.sendscaledphotososerver.activites;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import ru.devdem.sendscaledphotososerver.R;
 
 public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View view = View.inflate(this, R.layout.activity_register, null);
+        setContentView(view);
+
     }
 }
